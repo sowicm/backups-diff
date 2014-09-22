240ca62eb68265c8ab2ac4f50483c8c97f92fb0e
sowicm <sowicm@Sowicm-Air.lan>
2014-09-22 14:58:10 +0800

    Initial Commit

---
+++ BackupsDiff/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BackupsDiff
+//
+//  Created by Sowicm Right on 14/9/22.
+//  Copyright (c) 2014年 Sowicm Right. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BackupsDiff/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BackupsDiff
+//
+//  Created by Sowicm Right on 14/9/22.
+//  Copyright (c) 2014年 Sowicm Right. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
